50d0dd65773ba160f6cfcca4958a303954e69b5c
Tatiana Lavrovskaya <lavr39820@gmail.com>
2020-06-09 21:58:12 +0300

    Added form

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const express = require('express');
 const path = require('path');
 const exphbs = require('express-handlebars');
@@ -17,6 +19,7 @@ app.set('view engine', 'hbs');
 app.set('views', 'views');
 
 app.use(express.static('public'));
+app.use(express.urlencoded({extended: true}));
 app.use('/', homeRoutes);
 app.use('/add', addRoutes);
 app.use('/courses', coursesRoutes);
